fa40bf9e23e5fe5c931c85494fce8a0b7db2abd1
unknown <rf3020@acad.ucdb.br>
2019-10-01 18:52:27 -0400

    BodyParser

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+//app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
 app.listen(3000, function(){
     console.log('Server running on port 3000')
 })
 
-app.get('/',function (req,res){
-    res.send('Welcome!!!')
+app.get('/', (req,res)=>{
+    res.send('Welcome')
 })
+
+app.post('/', (req,res)=>{
+    res.send(req.body)
+})
+
